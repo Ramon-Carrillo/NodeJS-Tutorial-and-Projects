2e12b64dc8c7027ba368acd237364cd643715ce2
Ramon-Carrillo <carrillojrramon@gmail.com>
2022-07-25 23:03:17 -0500

    refactored the code, connected to mongodb, added .env file

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,28 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+
+//* middleware
+app.use(express.json())
+
+//* routes
+app.get('/', (req, res) => {
+  res.send('Task manager')
+})
+
+app.use('/api/v1/tasks', tasks)
+
+const port = 5000
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, () => console.log(`Server started on port ${port}`))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
