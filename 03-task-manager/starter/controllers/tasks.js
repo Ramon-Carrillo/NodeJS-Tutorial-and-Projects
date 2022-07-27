005607e4de51d810aea28309ff0f5e0c9415ad9c
Ramon-Carrillo <carrillojrramon@gmail.com>
2022-07-26 19:00:19 -0500

    finished basic function of the project.

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,21 +1,60 @@
-const getAllTasks = (req, res) => {
-  res.send('get all tasks')
+const Task = require('../models/Task')
+
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({})
+    res.json({ tasks })
+  } catch (error) {
+    res.status(500).send({ msg: error.message })
+  }
 }
 
-const createTask = (req, res) => {
-  res.json(req.body)
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body)
+    res.json({ task })
+  } catch (error) {
+    res.status(500).send({ msg: error.message })
+  }
 }
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id })
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params
+    const task = await Task.findOne({ _id: taskID })
+    res.json({ task })
+  } catch (error) {
+    res.status(404).send({ msg: error.message })
+  }
 }
 
-const updateTask = (req, res) => {
-  res.send('update task')
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params
+    const task = await Task.findOneAndDelete({ _id: taskID })
+    if (!task) {
+      return res.status(404).send({ msg: 'Task not found' })
+    }
+    res.json({ task })
+  } catch (error) {
+    res.status(500).send({ msg: error.message })
+  }
 }
 
-const deleteTask = (req, res) => {
-  res.send('delete task')
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    })
+    if (!task) {
+      return res.status(404).send({ msg: 'Task not found' })
+    }
+    res.json({ task })
+  } catch (error) {
+    res.status(500).send({ msg: error.message })
+  }
 }
 
 module.exports = {
